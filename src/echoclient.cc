d52a70e800f7ebd0e27245f19f9508f88193ddcf
Derek Huang <djh458@stern.nyu.edu>
2023-09-26 22:27:00 -0400

    add simple echoclient (no description) and update CMakeLists.txt
    src/CMakelists.txt
        Add echoclient target + link options for Windows
    
    src/echoclient.cc
        Add echo client that sends and waits for an echo

---
+++ src/echoclient.cc
@@ -0,0 +1,65 @@
+/**
+ * @file echoclient.cc
+ * @author Derek Huang
+ * @brief Toy echo client
+ * @copyright MIT License
+ */
+
+// on Windows, we use Windows Sockets 2
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <WinSock2.h>
+#else
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#endif  // !defined(_WIN32)
+
+#include <cerrno>
+#include <cstdio>
+#include <iostream>
+
+#define PDNNET_ADD_CLIOPT_HOST
+#define PDNNET_ADD_CLIOPT_PORT
+#define PDNNET_CLIOPT_PORT_DEFAULT 8888
+#define PDNNET_ADD_CLIOPT_MESSAGE_BYTES
+
+#include "pdnnet/cerror.h"
+#include "pdnnet/cliopt.h"
+#include "pdnnet/features.h"
+#include "pdnnet/socket.h"
+
+PDNNET_ARG_MAIN
+{
+  PDNNET_CLIOPT_PARSE_OPTIONS();
+  // open IPv4 TCP/IP socket + resolve IPv4 host
+  pdnnet::unique_socket socket{AF_INET, SOCK_STREAM};
+  // TODO: write C++ wrapper for this
+  auto serv_ent = gethostbyname(PDNNET_CLIOPT(host));
+  if (!serv_ent)
+#if defined(PDNNET_BSD_DEFAULT_SOURCE)
+    PDNNET_H_ERRNO_EXIT_EX(h_errno, "No such host %s", PDNNET_CLIOPT(host));
+#else
+    PDNNET_ERROR_EXIT_EX("No such host %s", PDNNET_CLIOPT(host));
+#endif  // !defined(PDNNET_BSD_DEFAULT_SOURCE)
+  // create socket address struct
+  auto serv_addr = pdnnet::socket_address(serv_ent, PDNNET_CLIOPT(port));
+  // attempt connection
+  if (!pdnnet::connect(socket, serv_addr))
+    PDNNET_ERRNO_EXIT(errno, "Could not connect to socket");
+  // read from stream and write to socket + signal end of transmission
+  std::cin >> pdnnet::socket_writer{socket};
+  pdnnet::shutdown(socket, pdnnet::shutdown_type::write);
+  // read from socket and write to output stream, include trailing newline
+  std::cout << pdnnet::socket_reader{socket} << std::endl;
+  // done, so close socket
+#if defined(_WIN32)
+  if (pdnnet::close_handle(socket) == SOCKET_ERROR)
+    PDNNET_ERROR_EXIT(pdnnet::winsock_error("Failed to close socket").c_str());
+#else
+  if (pdnnet::close_handle(socket) < 0)
+    PDNNET_ERRNO_EXIT(errno, "Failed to close socket");
+#endif  // !defined(_WIN32)
+  return EXIT_SUCCESS;
+}
