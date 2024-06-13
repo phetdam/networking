8b41a6d8490828ff01929f41ee64126ecf6ef8a0
Derek Huang <djh458@stern.nyu.edu>
2024-06-13 00:23:52 -0400

    remove extraneous includes
    modified:   src/echoclient.cc
    modified:   src/echoserver.cc

--- src/echoclient.cc
+++ src/echoclient.cc
@@ -5,17 +5,6 @@
  * @copyright MIT License
  */
 
-// on Windows, we use Windows Sockets 2
-#if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <WinSock2.h>
-#else
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#endif  // !defined(_WIN32)
-
 #include <cstdlib>
 #include <iostream>
 
--- src/echoserver.cc
+++ src/echoserver.cc
@@ -5,17 +5,6 @@
  * @copyright MIT License
  */
 
-#if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <WinSock2.h>
-#else
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <unistd.h>
-#endif  // !defined(_WIN32)
-
 #include <cstdlib>
 #include <iostream>
 #include <thread>
