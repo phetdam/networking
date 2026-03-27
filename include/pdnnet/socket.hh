db483fe5ab3245666305cffce59e9f9161ebf809
Derek Huang <djh458@stern.nyu.edu>
2026-03-26 23:10:42 -0400

    socket.hh: silence C4365 for MSVC

--- include/pdnnet/socket.hh
+++ include/pdnnet/socket.hh
@@ -1117,7 +1117,9 @@ public:
       // write to stream + clear buffer
       // FIXME: bytes are assumed to be in the same order as the receiving
       // machine. in general this will not provide the desired bytes
+PDNNET_MSVC_WARNING_DISABLE(4365)
       out.write(reinterpret_cast<const CharT*>(buf_.get()), n_read / sizeof(CharT));
+PDNNET_MSVC_WARNING_ENABLE()
       std::memset(buf_.get(), 0, buf_size_);
     }
     while (n_read);
